3f817c51759f9c71936ec6caafc24647bc5f9574
Timbuli Remus <kapucin28@gmail.com>
2016-06-15 12:29:36 +0300

    Adding menu variables

--- src/masterTable/MainMasterTable.java
+++ src/masterTable/MainMasterTable.java
@@ -1,5 +1,9 @@
 package masterTable;
 
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.layout.Pane;
 
 /**
@@ -13,6 +17,18 @@ public class MainMasterTable extends Pane {
     private final String post = "post";
     private final String ID = "ID";
     private final String team = "team";
+    //------------------------------------------------------------------------------------------------------------------
+
+    // Menu variables---------------------------------------------------------------------------------------------------
+    private final MenuBar menuBar = new MenuBar();
+    private final Menu fileMenu = new Menu("_File");
+    private final Menu editMenu = new Menu("_Edit");
+    private final MenuItem saveTable = new MenuItem("Save As...");
+    private final MenuItem loadTable = new MenuItem("Load...");
+    private final MenuItem refreshTable = new MenuItem("Refresh");
+    private final MenuItem clearTable = new MenuItem("Clear Table");
+    private final MenuItem exit = new MenuItem("Exit");
+    private final SeparatorMenuItem separator = new SeparatorMenuItem();
     //------------------------------------------------------------------------------------------------------------------
 
     // Constructor------------------------------------------------------------------------------------------------------
