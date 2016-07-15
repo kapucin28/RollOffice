c49136a88e085ffec8a4f64071c488305d5d4a40
kapucin28 <kapucin28@gmail.com>
2016-07-15 18:24:24 +0300

    Adding class description

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,6 +10,9 @@ import javafx.stage.Stage;
 
 /**
  * Created by TIMBULI REMUS K@puc!n on 01-Jun-16.
+ *
+ *      The main class where all the objects are
+ * displayed on stage
  */
 public class Main extends Application {
 
