ad1649b67b824fbfa85e4d557f7a770df6b80b4d
kapucin28 <kapucin28@gmail.com>
2016-07-17 09:20:24 +0300

    Adding class description

--- src/chartsGraphics/BarChartGraphic.java
+++ src/chartsGraphics/BarChartGraphic.java
@@ -4,6 +4,9 @@ import javafx.scene.layout.Pane;
 
 /**
  * Created by TIMBULI REMUS K@puc!n on 05-Jun-16.
+ *
+ *      This is the chart that will display
+ * combined results from all pie charts
  */
 public class BarChartGraphic extends Pane{
 
