d3695fe6c08fdb1b73dc5a226e00046be761e534
kapucin28 <kapucin28@gmail.com>
2016-07-19 10:56:45 +0300

    Adding class description

--- src/chartsGraphics/Table3PieChart.java
+++ src/chartsGraphics/Table3PieChart.java
@@ -4,6 +4,9 @@ import javafx.scene.layout.Pane;
 
 /**
  * Created by TIMBULI REMUS K@puc!n on 09-Jun-16.
+ *
+ *      This chart will represent the performance
+ * from team 3
  */
 public class Table3PieChart extends Pane {
 
