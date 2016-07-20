df093822f921561bc4c1c361600c63a94d49a940
kapucin28 <kapucin28@gmail.com>
2016-07-20 07:42:49 +0300

    Adding class description

--- src/chartsInfo/ChartsMasterInfoDetails.java
+++ src/chartsInfo/ChartsMasterInfoDetails.java
@@ -2,6 +2,9 @@ package chartsInfo;
 
 /**
  * Created by Timbuli Remus on 20.07.2016.
+ *
+ *      This class defines the information that
+ * will be used in the ChartsMasterInfo class
  */
 public class ChartsMasterInfoDetails {
 }
