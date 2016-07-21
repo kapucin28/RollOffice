527db7dd913e7610e365b0b34d01132844698e58
kapucin28 <kapucin28@gmail.com>
2016-07-21 07:47:06 +0300

    Adding class description

--- src/table1/MainTable1.java
+++ src/table1/MainTable1.java
@@ -4,6 +4,9 @@ import javafx.scene.layout.Pane;
 
 /**
  * Created by TIMBULI REMUS K@puc!n on 07-Jun-16.
+ *
+ *      This class represents the table for
+ * team 1
  */
 public class MainTable1 extends Pane {
 
