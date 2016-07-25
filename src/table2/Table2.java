16c8012c6eae1832b63d9bb672f0b6283c3ff2e5
kapucin28 <kapucin28@gmail.com>
2016-07-25 09:01:08 +0300

    Updating IDE

--- src/table2/Table2.java
+++ src/table2/Table2.java
@@ -2,6 +2,9 @@ package table2;
 
 /**
  * Created by TIMBULI REMUS K@puc!n on 07-Jun-16.
+ *
+ *     This class represents persons details that
+ * will be inserted in the table
  */
 public class Table2 {
 }
