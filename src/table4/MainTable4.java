f8ebdc3ef54bb9459154ea2ed0673810a16ca46d
kapucin28 <kapucin28@gmail.com>
2016-07-26 11:18:39 +0300

    Adding class description

--- src/table4/MainTable4.java
+++ src/table4/MainTable4.java
@@ -4,6 +4,9 @@ import javafx.scene.layout.Pane;
 
 /**
  * Created by TIMBULI REMUS K@puc!n on 07-Jun-16.
+ *
+ *      This class represents the table for
+ * team 4
  */
 public class MainTable4 extends Pane{
 
