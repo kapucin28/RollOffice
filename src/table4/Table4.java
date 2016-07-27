0f3de10de56922c03c85af121cca7b7356a3f3c4
kapucin28 <kapucin28@gmail.com>
2016-07-27 10:22:10 +0300

    Adding class description

--- src/table4/Table4.java
+++ src/table4/Table4.java
@@ -2,6 +2,9 @@ package table4;
 
 /**
  * Created by TIMBULI REMUS K@puc!n on 07-Jun-16.
+ *
+ *     This class represents persons details that
+ * will be inserted in table 4
  */
 public class Table4 {
 }
