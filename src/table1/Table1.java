99555b8df301423927049837fd345a998e0517a3
kapucin28 <kapucin28@gmail.com>
2016-08-11 19:13:24 +0300

    Adding performance details for table 1

--- src/table1/Table1.java
+++ src/table1/Table1.java
@@ -1,10 +1,19 @@
 package table1;
 
+import java.util.GregorianCalendar;
+
 /**
  * Created by TIMBULI REMUS K@puc!n on 07-Jun-16.
  *
- *     This class represents persons details that
+ *     This class represents performance details that
  * will be inserted in table 1
  */
 public class Table1 {
+
+    // Team1 variables--------------------------------------------------------------------------------------------------
+    private GregorianCalendar calendar = new GregorianCalendar();
+    private String post;
+    private long scrap, pending, output, target, month, year;
+    //------------------------------------------------------------------------------------------------------------------
+
 }
