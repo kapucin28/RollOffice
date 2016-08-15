bd5edb8bc118bce6007b9987fbd72310f0889c80
kapucin28 <kapucin28@gmail.com>
2016-08-15 10:47:10 +0300

    Generating getters & setters

--- src/table3/Table3.java
+++ src/table3/Table3.java
@@ -30,4 +30,61 @@ public class Table3 {
     }
     //------------------------------------------------------------------------------------------------------------------
 
+    // Getters & Setters------------------------------------------------------------------------------------------------
+    public String getPost() {
+        return post;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+
+    public long getScrap() {
+        return scrap;
+    }
+
+    public void setScrap(long scrap) {
+        this.scrap = scrap;
+    }
+
+    public long getPending() {
+        return pending;
+    }
+
+    public void setPending(long pending) {
+        this.pending = pending;
+    }
+
+    public long getOutput() {
+        return output;
+    }
+
+    public void setOutput(long output) {
+        this.output = output;
+    }
+
+    public long getTarget() {
+        return target;
+    }
+
+    public void setTarget(long target) {
+        this.target = target;
+    }
+
+    public long getMonth() {
+        return month;
+    }
+
+    public void setMonth(long month) {
+        this.month = month;
+    }
+
+    public long getYear() {
+        return year;
+    }
+
+    public void setYear(long year) {
+        this.year = year;
+    }
+    //------------------------------------------------------------------------------------------------------------------
 }
