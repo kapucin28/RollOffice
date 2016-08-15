57fd94094d8c9f38ab227b3d8b05b0b48694b667
kapucin28 <kapucin28@gmail.com>
2016-08-15 10:46:03 +0300

    Generating constructor

--- src/table3/Table3.java
+++ src/table3/Table3.java
@@ -16,4 +16,18 @@ public class Table3 {
     private long scrap, pending, output, target, month, year;
     //------------------------------------------------------------------------------------------------------------------
 
+    // Constructor------------------------------------------------------------------------------------------------------
+    public Table3(String post, long scrap, long pending, long output, long target, long month, long year) {
+        year = calendar.get(GregorianCalendar.YEAR);
+        month = calendar.get(GregorianCalendar.MONTH);
+        this.post = post;
+        this.scrap = scrap;
+        this.pending = pending;
+        this.output = output;
+        this.target = target;
+        this.month = month;
+        this.year = year;
+    }
+    //------------------------------------------------------------------------------------------------------------------
+
 }
