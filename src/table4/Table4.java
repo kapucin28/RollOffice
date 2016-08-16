30b400f56584f31de635cbce888ed0504f283089
kapucin28 <kapucin28@gmail.com>
2016-08-16 10:05:40 +0300

    Adding performance details for table 4

--- src/table4/Table4.java
+++ src/table4/Table4.java
@@ -1,10 +1,19 @@
 package table4;
 
+import java.util.GregorianCalendar;
+
 /**
  * Created by TIMBULI REMUS K@puc!n on 07-Jun-16.
  *
- *     This class represents persons details that
+ *     This class represents performance details that
  * will be inserted in table 4
  */
 public class Table4 {
+
+    // Team4 variables--------------------------------------------------------------------------------------------------
+    private GregorianCalendar calendar = new GregorianCalendar();
+    private String post;
+    private long scrap, pending, output, target, month, year;
+    //------------------------------------------------------------------------------------------------------------------
+
 }
