da4733ef14205f10bd6c110e324149215134202e
kapucin28 <kapucin28@gmail.com>
2016-08-18 00:30:14 +0300

    Updating table variables

--- src/table1/MainTable1.java
+++ src/table1/MainTable1.java
@@ -14,6 +14,16 @@ public class MainTable1 extends Pane {
     private Table1 table1 = new Table1("", 0, 0, 0, 0, 0, 0);
     //------------------------------------------------------------------------------------------------------------------
 
+    // Persons details variables----------------------------------------------------------------------------------------
+    private final String post = "post";
+    private final String scrap = "scrap";
+    private final String pending = "pending";
+    private final String output = "output";
+    private final String target = "target";
+    private final String month = "month";
+    private final String year = "year";
+    //------------------------------------------------------------------------------------------------------------------
+
     // Constructor------------------------------------------------------------------------------------------------------
     public MainTable1(){
 
