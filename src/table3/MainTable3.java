0585f16a5b7f0526c3263676d72835320f310bf9
kapucin28 <kapucin28@gmail.com>
2016-09-09 00:39:52 +0300

    Adding pane constants

--- src/table3/MainTable3.java
+++ src/table3/MainTable3.java
@@ -2,9 +2,12 @@ package table3;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.FileChooser;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -59,6 +62,12 @@ public class MainTable3 extends Pane{
     private Stage fileStage;
     private ObjectInputStream fromFile;
     private ObjectOutputStream toFile;
+    //------------------------------------------------------------------------------------------------------------------
+
+    // Panes variables--------------------------------------------------------------------------------------------------
+    private final Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
+    private final GridPane root = new GridPane();
+    private final GridPane tableGrid = new GridPane();
     //------------------------------------------------------------------------------------------------------------------
 
     // Constructor------------------------------------------------------------------------------------------------------
