b2c007aab69ef288b925d24b386db601b405e006
kapucin28 <kapucin28@gmail.com>
2016-09-30 00:17:38 +0300

    Adding chart variables

--- src/chartsGraphics/Table1PieChart.java
+++ src/chartsGraphics/Table1PieChart.java
@@ -1,6 +1,9 @@
 package chartsGraphics;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.chart.PieChart;
 import javafx.scene.layout.Pane;
 import javafx.stage.Screen;
 import table1.Table1;
@@ -24,6 +27,13 @@ public class Table1PieChart extends Pane {
     // Chart variables--------------------------------------------------------------------------------------------------
     private final Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
     private final Pane pane = new Pane();
+    private PieChart.Data scrapSlice = new PieChart.Data("Scrap", scrap);
+    private PieChart.Data pendingSlice = new PieChart.Data("Pending", pending);
+    private PieChart.Data outputSlice = new PieChart.Data("Output", output);
+    private PieChart.Data targetSlice = new PieChart.Data("Target", target);
+
+    private ObservableList<PieChart.Data> pie = FXCollections.observableArrayList();
+    private PieChart chart = new PieChart(pie);
     //------------------------------------------------------------------------------------------------------------------
 
     // Constructor------------------------------------------------------------------------------------------------------
