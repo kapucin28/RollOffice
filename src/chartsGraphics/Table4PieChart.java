f0b3410a16f6baa7329c9686964e96de68a1d14a
kapucin28 <kapucin28@gmail.com>
2016-10-14 00:07:40 +0300

    Adding table variables

--- src/chartsGraphics/Table4PieChart.java
+++ src/chartsGraphics/Table4PieChart.java
@@ -13,6 +13,10 @@ public class Table4PieChart extends Pane {
 
     // Table 4 variables------------------------------------------------------------------------------------------------
     private Table4 table4 = new Table4("", 0, 0, 0, 0, 0, 0);
+    private long scrap = table4.getScrap();
+    private long pending = table4.getPending();
+    private long output = table4.getOutput();
+    private long target = table4.getTarget();
     //------------------------------------------------------------------------------------------------------------------
 
     // Constructor------------------------------------------------------------------------------------------------------
