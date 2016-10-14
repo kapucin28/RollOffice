e166765a7308137b3b320edafe6ba846f9bbdc6c
kapucin28 <kapucin28@gmail.com>
2016-10-15 00:19:17 +0300

    Adding chart variables

--- src/chartsGraphics/Table4PieChart.java
+++ src/chartsGraphics/Table4PieChart.java
@@ -1,6 +1,8 @@
 package chartsGraphics;
 
+import javafx.geometry.Rectangle2D;
 import javafx.scene.layout.Pane;
+import javafx.stage.Screen;
 import table4.Table4;
 
 /**
@@ -17,6 +19,11 @@ public class Table4PieChart extends Pane {
     private long pending = table4.getPending();
     private long output = table4.getOutput();
     private long target = table4.getTarget();
+    //------------------------------------------------------------------------------------------------------------------
+
+    // Chart variables--------------------------------------------------------------------------------------------------
+    private final Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
+    private final Pane pane = new Pane();
     //------------------------------------------------------------------------------------------------------------------
 
     // Constructor------------------------------------------------------------------------------------------------------
