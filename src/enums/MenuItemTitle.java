4b0f847c6f26fefbd47f56e95662e16bb2a2ae25
kapucin28 <kapucin28@gmail.com>
2016-11-04 03:01:26 +0200

    Implementing enum

---
+++ src/enums/MenuItemTitle.java
@@ -0,0 +1,13 @@
+package enums;
+
+/**
+ * Created by Timbuli Remus on 04.11.2016.
+ * <p>
+ * Titles for menu items
+ */
+public enum MenuItemTitle {
+
+    _File, _Edit, SaveAs, Load,
+    Refresh, ClearTable, Exit
+
+}
