ad98a0046ee0cad0b829b34dc1e5282aa76aadce
kapucin28 <kapucin28@gmail.com>
2016-11-08 00:16:15 +0200

    Implementing enum

---
+++ src/enums/TableColumnsTextLink.java
@@ -0,0 +1,14 @@
+package enums;
+
+/**
+ * Created by Timbuli Remus on 08.11.2016.
+ *
+ *      Strings that link table columns to objects
+ */
+public enum TableColumnsTextLink {
+
+    Post, Scrap, Pending, Output,
+    Target, Month, Year, Name, Surname,
+    ID, Team
+
+}
