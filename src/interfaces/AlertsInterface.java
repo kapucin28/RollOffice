d1c619159d7b77b875d6588100e9872c60b6bad8
kapucin28 <kapucin28@gmail.com>
2016-11-16 01:44:06 +0200

    Implementing interface

---
+++ src/interfaces/AlertsInterface.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+/**
+ * Created by Timbuli Remus on 16.11.2016.
+ * <p>
+ * Interface for alerts
+ */
+public interface AlertsInterface {
+
+    String emptyAlertMessage = "Empty Table";
+    String exitProgramMessage = "Exit Program";
+    String streamAlertMessage = "Stream Error";
+}
