3eead02a1e5a5ba024db981d4f4a11c63a3c1d01
kapucin28 <kapucin28@gmail.com>
2016-11-18 22:00:57 +0200

    Implementing interface

---
+++ src/interfaces/ChartsTitles.java
@@ -0,0 +1,23 @@
+package interfaces;
+
+/**
+ * Created by Timbuli Remus on 18.11.2016.
+ * <p>
+ * Charts titles & strings
+ */
+public interface ChartsTitles {
+
+    String year1 = "2016";
+    String year2 = "2017";
+    String year3 = "2018";
+    String year4 = "2019";
+    String total = "TOTAL";
+    String overall = "Overall";
+    String performanceTitle = "Performance";
+    String teamsTitle = "Teams";
+    String scrapSlice = "Scrap";
+    String pendingSlice = "Pending";
+    String outputSlice = "Output";
+    String targetSlice = "Target";
+
+}
