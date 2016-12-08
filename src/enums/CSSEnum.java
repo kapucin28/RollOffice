e4e323cbb86f58bd53bdc7621a00377e34c4ce76
kapucin28 <kapucin28@gmail.com>
2016-12-09 00:32:15 +0200

    Implementing enum

--- src/enums/CSSEnum.java
+++ src/enums/CSSEnum.java
@@ -9,6 +9,8 @@ public enum CSSEnum {
 
     mainPane, menuBar, fileMenu, editMenu, saveTable, loadTable,
     refreshTable, clearTable, exit, separatorMasterTable, tableView,
-
+    nameColumn, surnameColumn, postColumn, IDColumn, teamColumn,
+    tfName, tfSurname, tfPost, tfID, tfTeam, addButton, removeButton,
+    MasterRootPane, tableGrid, inputGrid
 
 }
