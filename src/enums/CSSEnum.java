3f4b6632bda435bc437c630a72d99207cb189beb
kapucin28 <kapucin28@gmail.com>
2016-12-09 00:29:39 +0200

    Implementing enum

---
+++ src/enums/CSSEnum.java
@@ -0,0 +1,14 @@
+package enums;
+
+/**
+ * Created by Timbuli Remus on 09.12.2016.
+ * <p>
+ * Enum for CSS style of objects
+ */
+public enum CSSEnum {
+
+    mainPane, menuBar, fileMenu, editMenu, saveTable, loadTable,
+    refreshTable, clearTable, exit, separatorMasterTable, tableView,
+
+
+}
